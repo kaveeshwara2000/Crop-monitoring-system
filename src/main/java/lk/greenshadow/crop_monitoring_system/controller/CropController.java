af79302b24da67e10904aa6a4d81f8e587a049ff
kaveeshwara2000 <sachindukaveeshwara@gmail.com>
2024-12-07 09:27:25 +0530

    new update

--- src/main/java/lk/greenshadow/crop_monitoring_system/controller/CropController.java
+++ src/main/java/lk/greenshadow/crop_monitoring_system/controller/CropController.java
@@ -8,16 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
-import java.nio.file.Path;
+import java.nio.file.*;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.Date;
@@ -67,8 +64,103 @@ public class CropController {
             e.printStackTrace();
         }
 
+        Crop crop = new Crop();
+        crop.setCode(cropDTO.getCode());
+        crop.setcName(cropDTO.getcName());
+        crop.setsName(cropDTO.getsName());
+        crop.setImage(storageFileName);
+        crop.setCategory(cropDTO.getCategory());
+        crop.setSeason(cropDTO.getSeason());
+        crop.setField(cropDTO.getField());
+
+        cropRepo.save(crop);
+
         return "redirect:/crop";
     }
 
+    @GetMapping("/edit")
+    public String showEditPage(Model model, @RequestParam String code){
+        try {
+            Crop crop = cropRepo.findById(code).get();
+            model.addAttribute("crop",crop);
+
+            CropDTO cropDTO = new CropDTO();
+            cropDTO.setcName(crop.getcName());
+            cropDTO.setsName(crop.getsName());
+            cropDTO.setCategory(crop.getCategory());
+            cropDTO.setSeason(crop.getSeason());
+            cropDTO.setField(crop.getField());
+
+            model.addAttribute("cropDTO",cropDTO);
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return "redirect:/crop";
+        }
+        return "crop/editCrop";
+    }
+
+    @PostMapping("/edit")
+    public String updateCrop(Model model,@RequestParam String code,@Valid @ModelAttribute CropDTO cropDTO,BindingResult result){
+
+        try {
+            Crop crop = cropRepo.findById(code).get();
+            model.addAttribute("crop",crop);
+
+            if (result.hasErrors()){
+                return "crop/editCrop";
+            }
+            if (!cropDTO.getImage().isEmpty()){
+                String uploadDir = "public/images/";
+                Path oldImagePath = Paths.get(uploadDir+crop.getImage());
+                try {
+                    Files.delete(oldImagePath);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                MultipartFile image = cropDTO.getImage();
+                Date createdAt = new Date();
+                String storageFileName = createdAt.getTime()+"_"+image.getOriginalFilename();
+                try(InputStream inputStream = image.getInputStream()) {
+                    Files.copy(inputStream,Paths.get(uploadDir+storageFileName),StandardCopyOption.REPLACE_EXISTING);
+                }
+                crop.setImage(storageFileName);
+            }
+            crop.setcName(cropDTO.getcName());
+            crop.setsName(cropDTO.getsName());
+            crop.setCategory(cropDTO.getCategory());
+            crop.setSeason(cropDTO.getSeason());
+            crop.setField(cropDTO.getField());
+
+            cropRepo.save(crop);
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+
+        return "redirect:/crop";
+    }
+
+
+
+    @GetMapping("/delete")
+    public String deleteCrop(@RequestParam String code){
+
+        try {
+            Crop crop = cropRepo.findById(code).get();
+            Path imagePath = Paths.get("public/images/"+crop.getImage());
+
+            try {
+                Files.delete(imagePath);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            cropRepo.delete(crop);
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+
+        return "redirect:/crop";
+    }
 
 }
--- src/main/java/lk/greenshadow/crop_monitoring_system/controller/EquipmentController.java
+++ src/main/java/lk/greenshadow/crop_monitoring_system/controller/EquipmentController.java
@@ -1,4 +1,78 @@
 package lk.greenshadow.crop_monitoring_system.controller;
 
+import jakarta.validation.Valid;
+import lk.greenshadow.crop_monitoring_system.dto.CropDTO;
+import lk.greenshadow.crop_monitoring_system.dto.EquipmentDTO;
+import lk.greenshadow.crop_monitoring_system.model.Crop;
+import lk.greenshadow.crop_monitoring_system.model.Equipment;
+import lk.greenshadow.crop_monitoring_system.service.EquipmentRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/equipment")
 public class EquipmentController {
+
+    @Autowired
+    private EquipmentRepo equipmentRepo;
+
+    @GetMapping({"","/"})
+    public String showEquipmentList(Model model){
+        List<Equipment> equipment = equipmentRepo.findAll();
+        model.addAttribute("equipment",equipment);
+        return "equipment/index";
+    }
+
+    @GetMapping("/create")
+    public String showCreatePage(Model model){
+        EquipmentDTO equipmentDTO = new EquipmentDTO();
+        model.addAttribute("equipmentDTO",equipmentDTO);
+        return "equipment/createEquipment";
+    }
+
+    @PostMapping("/create")
+    public String createEquipment(@Valid @ModelAttribute EquipmentDTO equipmentDTO, BindingResult result){
+        if (result.hasErrors()){
+            return "crop/createCrop";
+        }
+
+
+        Equipment equipment = new Equipment();
+        equipment.setId(equipmentDTO.getId());
+        equipment.setName(equipmentDTO.getName());
+        equipment.setType(equipmentDTO.getType());
+        equipment.setStatus(equipmentDTO.getStatus());
+        equipment.setField(equipmentDTO.getField());
+
+        equipmentRepo.save(equipment);
+
+        return "redirect:/crop";
+    }
+
+    @GetMapping("/edit")
+    public String showEditPage(Model model, @RequestParam String id){
+        try {
+            Equipment equipment = equipmentRepo.findById(id).get();
+            model.addAttribute("equipment",equipment);
+
+            EquipmentDTO equipmentDTO = new EquipmentDTO();
+            equipmentDTO.setName(equipment.getName());
+            equipmentDTO.setType(equipment.getType());
+            equipmentDTO.setStatus(equipment.getStatus());
+            equipmentDTO.setField(equipment.getField());
+
+            model.addAttribute("equipmentDTO",equipmentDTO);
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return "redirect:/equipment";
+        }
+        return "equipment/editEquipment";
+    }
+
 }
